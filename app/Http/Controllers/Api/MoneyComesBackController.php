38d9f10f8313dc64bed28bbd5fe120a021cb147a
minhtv <tranvanminh30398@gmail.com>
2024-07-02 20:14:42 +0700

    update

--- app/Http/Controllers/Api/MoneyComesBackController.php
+++ app/Http/Controllers/Api/MoneyComesBackController.php
@@ -101,6 +101,8 @@ class MoneyComesBackController extends Controller
         $params['date_to'] = str_replace('/', '-', $params['date_to']);
 
         $params_transfer['agent_id'] = $params['agent_id'];
+        $params_transfer['agent_date_to'] =  $params['date_to'];
+        $params_transfer['agent_date_from'] =  $params['date_from'];
 
         $data = $this->money_repo->getListingAgent($params, false, true);
         $total = $this->money_repo->getListingAgent($params, true, true);
